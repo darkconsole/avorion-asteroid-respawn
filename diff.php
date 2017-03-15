2582346880b11cbcfc7efcf90bef53f0061d3c17
darkconsole <darkcee.legit@gmail.com>
2017-03-14 20:14:20 -0500

    bring my lazy tools in

---
+++ diff.php
@@ -0,0 +1,51 @@
+<?php
+
+// this script will generate all the diffs that can be used for patching the
+// avorion source with these modifications.
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+define('ProjectRoot','..');
+define('StockDir', '/avorion-0.10.2/data/scripts');
+define('ModDir', '/avorion-asteroid-respawn/data/scripts');
+define('PatchDir', '/avorion-asteroid-respawn/patches');
+
+define('Files',[
+	'/dcc-asteroid-respawn.lua' => '/patch-dcc-asteroid-respawn.diff'
+]);
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+function
+Pathify(String $Filepath):
+String {
+/*//
+generate proper file paths for the os given that we are writing the code for
+forward slashes in mind. seems to be needed for some windows commands.
+//*/
+
+	$Filepath = str_replace('%VERSION%','Version',$Filepath);
+
+	return str_replace('/',DIRECTORY_SEPARATOR,$Filepath);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+$File;
+$Patch;
+$Command;
+
+foreach(Files as $File => $Patch) {
+	$Command = sprintf(
+		'diff -urN %s %s > %s',
+		escapeshellarg(Pathify(ProjectRoot.StockDir.$File)),
+		escapeshellarg(Pathify(ProjectRoot.ModDir.$File)),
+		escapeshellarg(Pathify(ProjectRoot.PatchDir.$Patch))
+	);
+
+	echo $Command, PHP_EOL;
+	system($Command);
+}
---
+++ push.php
@@ -0,0 +1,50 @@
+<?php
+
+// this script will copy the files to our server via my remote ssh mount.
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+define('ProjectRoot','..');
+define('StockDir', '/avorion-0.10.2/data/scripts');
+define('ModDir', '/avorion-asteroid-respawn/data/scripts');
+define('PatchDir', '/avorion-asteroid-respawn/patches');
+define('RemoteDir','z:\home\a1270\steam_games\avorion\data\scripts');
+
+define('Files',[
+	'/dcc-asteroid-respawn.lua' => '/patch-dcc-asteroid-respawn.diff'
+]);
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+function
+Pathify(String $Filepath):
+String {
+/*//
+generate proper file paths for the os given that we are writing the code for
+forward slashes in mind. seems to be needed for some windows commands.
+//*/
+
+	$Filepath = str_replace('%VERSION%','Version',$Filepath);
+
+	return str_replace('/',DIRECTORY_SEPARATOR,$Filepath);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+$File;
+$Patch;
+$Command;
+
+foreach(Files as $File => $Patch) {
+	$Command = sprintf(
+		'xcopy /R /Y %s %s',
+		escapeshellarg(Pathify(ProjectRoot.ModDir.$File)),
+		escapeshellarg(Pathify(RemoteDir.$File))
+	);
+
+	echo $Command, PHP_EOL;
+	system($Command);
+}
